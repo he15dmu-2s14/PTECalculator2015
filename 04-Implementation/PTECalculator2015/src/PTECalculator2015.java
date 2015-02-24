87e7c4d8a173d4b1b7ba5ce96b3e7d2ba7e3ca0e
Enok Mikkelsen <enokmik@gmail.com>
2015-02-24 14:26:28 +0100

    Added a bit to Main, to test commit/merge

--- 04-Implementation/PTECalculator2015/src/PTECalculator2015.java
+++ 04-Implementation/PTECalculator2015/src/PTECalculator2015.java
@@ -4,6 +4,10 @@ public class PTECalculator2015 {
   public static void main( String[] args ) {
     // TODO Auto-generated method stub
 
+    String Hey = "Hej Hans!";
+    
+    System.out.println( Hey );
+        
   }
 
 }
