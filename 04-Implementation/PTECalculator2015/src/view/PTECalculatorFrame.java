ed86366bbe2c155606f6fff66dc33d8be8a0115e
meilby44411 <lassemeilby444@hotmail.com>
2015-02-27 12:50:13 +0100

    tilføjet focuslistener

--- 04-Implementation/PTECalculator2015/src/view/PTECalculatorFrame.java
+++ 04-Implementation/PTECalculator2015/src/view/PTECalculatorFrame.java
@@ -3,18 +3,22 @@ package view;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JRadioButton;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import logic.PTECalculatorController;
 import logic.PTECalculatorControllerStub;
 import logic.PTEObserver;
+import logic.UgyldigBelastningException;
 
-public class PTECalculatorFrame extends JFrame implements PTEObserver {
+public class PTECalculatorFrame extends JFrame implements PTEObserver,
+		FocusListener {
 	private JTextField belastning, belastningFormel, fDim, vinkel;
 	private JComboBox enhed, vandretLodret;
 	private JTextField fnFormel, ftFormel, fnResultat, ftResultat;
@@ -24,7 +28,7 @@ public class PTECalculatorFrame extends JFrame implements PTEObserver {
 	public PTECalculatorFrame() {
 		setTitle("PTECalculator");
 
-		pteCalc = new PTECalculatorControllerStub();// new PTECalculatorControllerImpl();
+		pteCalc = new PTECalculatorControllerImpl();
 		pteCalc.tilmeldObserver(this);
 
 		layout = new GridBagLayout();
@@ -53,6 +57,7 @@ public class PTECalculatorFrame extends JFrame implements PTEObserver {
 		con = createGBC(1, 1, 1, 1);
 		con.insets = ins;
 		belastning = new JTextField(4);
+		belastning.addFocusListener(this);
 		add(belastning, con);
 
 		con = createGBC(2, 1, 1, 1);
@@ -140,7 +145,8 @@ public class PTECalculatorFrame extends JFrame implements PTEObserver {
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+		setLocationRelativeTo(null);
+
 		update();
 	}
 
@@ -172,15 +178,53 @@ public class PTECalculatorFrame extends JFrame implements PTEObserver {
 			belastning.setText("" + pteCalc.getBelastning().getBelastning());
 			break;
 		}
-		
+
 		fDim.setText("" + pteCalc.getBelastning().getBelastning() + " N");
 
-		if(pteCalc.getVinkel().tilVandret()){
+		if (pteCalc.getVinkel().tilVandret()) {
 			vandretLodret.setSelectedItem("Vandret");
-		}
-		else 
+		} else
 			vandretLodret.setSelectedItem("Lodret");
-			
 
+	}
+
+	@Override
+	public void focusGained(FocusEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		if (e.getSource() == belastning) {
+
+			try {
+				double temp;
+				temp = Double.parseDouble(belastning.getText());
+				if (temp == Double.NaN || temp == Double.POSITIVE_INFINITY)
+					throw new NumberFormatException();
+
+				logic.Enhed unit = (logic.Enhed) enhed.getSelectedItem();
+
+				pteCalc.angivBelastning(temp, unit);
+
+			} catch (NumberFormatException | UgyldigBelastningException e2) {
+				JOptionPane.showMessageDialog(null, "Ugyldigt indput");
+			}
+		}
+
+		if (e.getSource() == vinkel) {
+			try {
+				double temp;
+				temp = Double.parseDouble(vinkel.getText());
+				
+				if(vandretLodret.getSelectedItem().toString() == "Vandret")
+					pteCalc.beregnTvaerkraft(temp, true);
+				else
+					pteCalc.beregnTvaerkraft(temp, false);
+			} catch (Exception e2) {
+				JOptionPane.showMessageDialog(null, "Ugyldigt indput");
+			}
+		}
 	}
 }
