63929a877ace559e94fc033044af26bbe157c8da
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-03-02 18:17:10 +0100

    Code cleanup

--- 04-Implementation/PTECalculator2015/src/view/PTECalculatorFrame.java
+++ 04-Implementation/PTECalculator2015/src/view/PTECalculatorFrame.java
@@ -12,232 +12,238 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
+import logic.Enhed;
 import logic.PTECalculatorController;
 import logic.PTECalculatorControllerImpl;
-import logic.PTECalculatorControllerStub;
 import logic.PTEObserver;
 import logic.UgyldigBelastningException;
 
-public class PTECalculatorFrame extends JFrame implements PTEObserver,
-		FocusListener {
-	private JTextField belastning, belastningFormel, fDim, vinkel;
-	private JComboBox enhed, vandretLodret;
-	private JTextField fnFormel, ftFormel, fnResultat, ftResultat;
-	private GridBagLayout layout;
-	private PTECalculatorController pteCalc;
+public class PTECalculatorFrame extends JFrame implements
+      PTEObserver,
+      FocusListener {
+   private static final long serialVersionUID = 1L;
+   private static final String VANDRET = "Vandret", LODRET = "Lodret";
 
-	public PTECalculatorFrame() {
-		setTitle("PTECalculator");
+   private JTextField belastning, belastningFormel, fDim, vinkel;
+   private JComboBox<Enhed> enhed;
+   private JComboBox<String> vandretLodret;
+   private JTextField fnFormel, ftFormel, fnResultat, ftResultat;
+   private PTECalculatorController pteCalc;
 
-		pteCalc = new PTECalculatorControllerImpl();
-		pteCalc.tilmeldObserver(this);
+   public PTECalculatorFrame() {
+      pteCalc = new PTECalculatorControllerImpl();
+      pteCalc.tilmeldObserver(this);
 
-		layout = new GridBagLayout();
-		getContentPane().setLayout(layout);
+      initComponents();
+      layoutComponents();
 
-		GridBagConstraints con = new GridBagConstraints();
-		Insets ins = new Insets(5, 5, 5, 5);
+      // settings på framen
+      setTitle("PTECalculator");
+      setSize(500, 300);
+      setResizable(false);
+      setVisible(true);
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+      setLocationRelativeTo(null);
 
-		// Linie 0
-		con = createGBC(3, 0, 1, 1);
-		con.insets = new Insets(5, 5, 0, 5);
-		con.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Formel"), con);
+      update();
+   }
 
-		con = createGBC(4, 0, 1, 1);
-		con.insets = new Insets(5, 5, 0, 5);
-		con.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Fdim"), con);
+   private void initComponents() {
+      belastning = new JTextField(4);
+      belastning.addFocusListener(this);
 
-		// Linie 1
-		con = createGBC(0, 1, 1, 1);
-		con.insets = ins;
-		con.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Belastning (Fdim):  "), con);
+      enhed = new JComboBox<Enhed>();
+      enhed.addItem(Enhed.kg);
+      enhed.addItem(Enhed.ton);
+      enhed.addItem(Enhed.Newton);
 
-		con = createGBC(1, 1, 1, 1);
-		con.insets = ins;
-		belastning = new JTextField(4);
-		belastning.addFocusListener(this);
-		add(belastning, con);
+      belastningFormel = new JTextField(8);
+      belastningFormel.setEditable(false);
 
-		con = createGBC(2, 1, 1, 1);
-		con.insets = ins;
-		enhed = new JComboBox();
-		enhed.addItem(logic.Enhed.kg);
-		enhed.addItem(logic.Enhed.ton);
-		enhed.addItem(logic.Enhed.Newton);
-		add(enhed, con);
+      fDim = new JTextField(5);
+      fDim.setEditable(false);
 
-		con = createGBC(3, 1, 1, 1);
-		con.insets = new Insets(0, 5, 5, 5);
-		belastningFormel = new JTextField(8);
-		belastningFormel.setEditable(false);
-		add(belastningFormel, con);
+      vinkel = new JTextField(4);
+      vinkel.addFocusListener(this);
 
-		con = createGBC(4, 1, 1, 1);
-		con.insets = new Insets(0, 5, 5, 5);
-		fDim = new JTextField(5);
-		fDim.setEditable(false);
-		add(fDim, con);
+      vandretLodret = new JComboBox<>();
+      vandretLodret.addItem(VANDRET);
+      vandretLodret.addItem(LODRET);
 
-		// Linie 2
-		con = createGBC(0, 2, 1, 1);
-		con.insets = new Insets(5, 5, 20, 5);
-		con.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Vinkel: "), con);
+      fnFormel = new JTextField(8);
+      fnFormel.setEditable(false);
 
-		con = createGBC(1, 2, 1, 1);
-		con.insets = new Insets(5, 5, 20, 5);
-		vinkel = new JTextField(4);
-		vinkel.addFocusListener(this);
-		add(vinkel, con);
+      fnResultat = new JTextField(4);
+      fnResultat.setEditable(false);
 
-		con = createGBC(2, 2, 1, 1);
-		con.insets = new Insets(5, 5, 20, 5);
-		vandretLodret = new JComboBox();
-		vandretLodret.addItem("Vandret");
-		vandretLodret.addItem("Lodret");
-		add(vandretLodret, con);
+      ftFormel = new JTextField(8);
+      ftFormel.setEditable(false);
 
-		// Linie 3
-		con = createGBC(1, 3, 1, 1);
-		con.insets = new Insets(5, 5, 0, 5);
-		con.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Formel"), con);
+      ftResultat = new JTextField(4);
+      ftResultat.setEditable(false);
+   }
 
-		// Linie 4
-		con = createGBC(0, 4, 1, 1);
-		con.insets = ins;
-		con.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Fn: "), con);
+   private void layoutComponents() {
+      getContentPane().setLayout(new GridBagLayout());
 
-		con = createGBC(1, 4, 1, 1);
-		con.insets = ins;
-		fnFormel = new JTextField(8);
-		fnFormel.setEditable(false);
-		add(fnFormel, con);
+      GridBagConstraints con;
+      Insets ins = new Insets(5, 5, 5, 5);
 
-		con = createGBC(2, 4, 1, 1);
-		con.insets = ins;
-		fnResultat = new JTextField(4);
-		fnResultat.setEditable(false);
-		add(fnResultat, con);
+      // Linie 0
+      con = createGBC(3, 0, 1, 1);
+      con.insets = new Insets(5, 5, 0, 5);
+      con.anchor = GridBagConstraints.WEST;
+      add(new JLabel("Formel"), con);
 
-		// Linie 5
-		con = createGBC(0, 5, 1, 1);
-		con.insets = ins;
-		con.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Ft: "), con);
+      con = createGBC(4, 0, 1, 1);
+      con.insets = new Insets(5, 5, 0, 5);
+      con.anchor = GridBagConstraints.WEST;
+      add(new JLabel("Fdim"), con);
 
-		con = createGBC(1, 5, 1, 1);
-		con.insets = ins;
-		ftFormel = new JTextField(8);
-		ftFormel.setEditable(false);
-		add(ftFormel, con);
+      // Linie 1
+      con = createGBC(0, 1, 1, 1);
+      con.insets = ins;
+      con.anchor = GridBagConstraints.WEST;
+      add(new JLabel("Belastning (Fdim):  "), con);
 
-		con = createGBC(2, 5, 1, 1);
-		con.insets = ins;
-		ftResultat = new JTextField(4);
-		ftResultat.setEditable(false);
-		add(ftResultat, con);
+      con = createGBC(1, 1, 1, 1);
+      con.insets = ins;
+      add(belastning, con);
 
-		// settings på framen
-		setSize(500, 300);
-		setResizable(false);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
+      con = createGBC(2, 1, 1, 1);
+      con.insets = ins;
+      add(enhed, con);
 
-		update();
-	}
+      con = createGBC(3, 1, 1, 1);
+      con.insets = new Insets(0, 5, 5, 5);
+      add(belastningFormel, con);
 
-	public GridBagConstraints createGBC(int x, int y, int width, int height) {
-		GridBagConstraints gbc = new GridBagConstraints();
+      con = createGBC(4, 1, 1, 1);
+      con.insets = new Insets(0, 5, 5, 5);
+      add(fDim, con);
 
-		gbc.gridx = x;
-		gbc.gridy = y;
+      // Linie 2
+      con = createGBC(0, 2, 1, 1);
+      con.insets = new Insets(5, 5, 20, 5);
+      con.anchor = GridBagConstraints.WEST;
+      add(new JLabel("Vinkel: "), con);
 
-		gbc.gridwidth = width;
-		gbc.gridheight = height;
+      con = createGBC(1, 2, 1, 1);
+      con.insets = new Insets(5, 5, 20, 5);
+      add(vinkel, con);
 
-		return gbc;
-	}
+      con = createGBC(2, 2, 1, 1);
+      con.insets = new Insets(5, 5, 20, 5);
+      add(vandretLodret, con);
 
-	@Override
-	public void update() {
-		String belastningText = "";
-		switch ((logic.Enhed) enhed.getSelectedItem()) {
-		case kg:
-			if (pteCalc.getBelastning() != null)
-				belastningText = "" + pteCalc.getBelastning().getBelastningIKg();
-			belastning.setText(belastningText);
-			break;
+      // Linie 3
+      con = createGBC(1, 3, 1, 1);
+      con.insets = new Insets(5, 5, 0, 5);
+      con.anchor = GridBagConstraints.WEST;
+      add(new JLabel("Formel"), con);
 
-		case ton:
-			if (pteCalc.getBelastning() != null)
-				belastningText = "" + pteCalc.getBelastning().getBelastningITon();
-			belastning.setText(belastningText);
-			break;
+      // Linie 4
+      con = createGBC(0, 4, 1, 1);
+      con.insets = ins;
+      con.anchor = GridBagConstraints.WEST;
+      add(new JLabel("Fn: "), con);
 
-		default:
-			if (pteCalc.getBelastning() != null)
-				belastningText = "" + pteCalc.getBelastning().getBelastning();
-			belastning.setText(belastningText);
-			break;
-		}
-		
-		if (pteCalc.getBelastning() != null)
-			fDim.setText("" + pteCalc.getBelastning().getBelastning() + " N");
-		
-		if (pteCalc.getVinkel() != null)
-			if (pteCalc.getVinkel().tilVandret()) {
-				vandretLodret.setSelectedItem("Vandret");
-			} else
-				vandretLodret.setSelectedItem("Lodret");
-		
-		if (pteCalc.getTvaerkraft() != null)
-			ftResultat.setText("" + pteCalc.getTvaerkraft().getTvaerkraft());
+      con = createGBC(1, 4, 1, 1);
+      con.insets = ins;
+      add(fnFormel, con);
 
-	}
+      con = createGBC(2, 4, 1, 1);
+      con.insets = ins;
+      add(fnResultat, con);
 
-	@Override
-	public void focusGained(FocusEvent e) {
-		// TODO Auto-generated method stub
+      // Linie 5
+      con = createGBC(0, 5, 1, 1);
+      con.insets = ins;
+      con.anchor = GridBagConstraints.WEST;
+      add(new JLabel("Ft: "), con);
 
-	}
+      con = createGBC(1, 5, 1, 1);
+      con.insets = ins;
+      add(ftFormel, con);
 
-	@Override
-	public void focusLost(FocusEvent e) {
-		if (e.getSource() == belastning) {
+      con = createGBC(2, 5, 1, 1);
+      con.insets = ins;
+      add(ftResultat, con);
+   }
 
-			try {
-				double temp;
-				temp = Double.parseDouble(belastning.getText());
-				if (temp == Double.NaN || temp == Double.POSITIVE_INFINITY)
-					throw new NumberFormatException();
+   public GridBagConstraints createGBC(int x, int y, int width, int height) {
+      GridBagConstraints gbc = new GridBagConstraints();
 
-				logic.Enhed unit = (logic.Enhed) enhed.getSelectedItem();
+      gbc.gridx = x;
+      gbc.gridy = y;
 
-				pteCalc.angivBelastning(temp, unit);
+      gbc.gridwidth = width;
+      gbc.gridheight = height;
 
-			} catch (NumberFormatException | UgyldigBelastningException e2) {
-				JOptionPane.showMessageDialog(null, "Ugyldigt indput");
-			}
-		}
+      return gbc;
+   }
 
-		if (e.getSource() == vinkel) {
-			try {
-				double temp;
-				temp = Double.parseDouble(vinkel.getText());
-				
-				if(vandretLodret.getSelectedItem().toString() == "Vandret")
-					pteCalc.beregnTvaerkraft(temp, true);
-				else
-					pteCalc.beregnTvaerkraft(temp, false);
-			} catch (Exception e2) {
-				JOptionPane.showMessageDialog(null, "Ugyldigt indput");
-			}
-		}
-	}
+   @Override
+   public void update() {
+      String belastningText = "";
+
+      Enhed enh = (Enhed) enhed.getSelectedItem();
+      switch (enh) {
+         case kg:
+            if (pteCalc.getBelastning() != null)
+               belastningText += pteCalc.getBelastning().getBelastningIKg();
+
+            belastning.setText(belastningText);
+            break;
+         case ton:
+            if (pteCalc.getBelastning() != null)
+               belastningText += pteCalc.getBelastning().getBelastningITon();
+
+            belastning.setText(belastningText);
+            break;
+         default:
+            if (pteCalc.getBelastning() != null)
+               belastningText += pteCalc.getBelastning().getBelastning();
+
+            belastning.setText(belastningText);
+            break;
+      }
+
+      if (pteCalc.getBelastning() != null)
+         fDim.setText(pteCalc.getBelastning().getBelastning() + " N");
+
+      if (pteCalc.getVinkel() != null)
+         vandretLodret.setSelectedItem(pteCalc.getVinkel().tilVandret()
+               ? VANDRET
+               : LODRET);
+
+      if (pteCalc.getTvaerkraft() != null)
+         ftResultat.setText(pteCalc.getTvaerkraft().getTvaerkraft() + " N");
+   }
+
+   @Override
+   public void focusGained(FocusEvent e) {
+      // No-op
+   }
+
+   @Override
+   public void focusLost(FocusEvent e) {
+      if (e.getSource() == belastning) {
+         try {
+            double b = Double.parseDouble(belastning.getText());
+            Enhed enh = (Enhed) enhed.getSelectedItem();
+            pteCalc.angivBelastning(b, enh);
+         } catch (NumberFormatException | UgyldigBelastningException ex) {
+            JOptionPane.showMessageDialog(null, "Ugyldigt input");
+         }
+      } else if (e.getSource() == vinkel) {
+         try {
+            double v = Double.parseDouble(vinkel.getText());
+            boolean vandret = vandretLodret.getSelectedItem().equals(VANDRET);
+            pteCalc.beregnTvaerkraft(v, vandret);
+         } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(null, "Ugyldigt input");
+         }
+      }
+   }
 }
