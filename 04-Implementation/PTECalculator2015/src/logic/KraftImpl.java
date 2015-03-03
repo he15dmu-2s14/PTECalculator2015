55e6441450de3593c4fbaaecf97a97b07b8db7da
NiemannLouise <Niemannlouise@gmail.com>
2015-03-03 14:02:37 +0100

    Tilføjelse af KraftImpl-Klassen. Igen

---
+++ 04-Implementation/PTECalculator2015/src/logic/KraftImpl.java
@@ -0,0 +1,17 @@
+package logic;
+
+public class KraftImpl implements Kraft {
+	protected Vinkel vinkel;
+	protected Belastning belastning;
+
+	@Override
+	public void setVinkel(Vinkel vinkel) {
+		this.vinkel = vinkel;
+	}
+
+	@Override
+	public void setBelastning(Belastning belastning) {
+		this.belastning = belastning;
+	}
+
+}
