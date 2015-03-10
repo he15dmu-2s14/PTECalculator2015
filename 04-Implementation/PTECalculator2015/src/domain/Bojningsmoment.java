92af0d79086a9ed69881cdcdf38225e8ba183212
TroyTroestrup <ATBusiness@live.dk>
2015-03-10 12:49:22 +0100

    Interface Bøjningsmoment opdateret
    + Implementation og UgyldigLængdeException

--- 04-Implementation/PTECalculator2015/src/domain/Bojningsmoment.java
+++ 04-Implementation/PTECalculator2015/src/domain/Bojningsmoment.java
@@ -2,7 +2,13 @@ package domain;
 
 public interface Bojningsmoment {
 
-	public void beregnSigmaBoj(Inertimoment i, Halvhojde e, double MB);
+	public void beregnSigmaBoj(double i, double e, double MB);
 	
 	public double getSigmaBoj();
+	
+	public void setArmLaengde( double mm );
+	
+	public double beregnMb( double ft, double mm );
+	
+	public double getMb();
 }
--- 04-Implementation/PTECalculator2015/src/domain/BojningsmomentImpl.java
+++ 04-Implementation/PTECalculator2015/src/domain/BojningsmomentImpl.java
@@ -2,15 +2,37 @@ package domain;
 
 public class BojningsmomentImpl implements Bojningsmoment {
 
-	double sigmaBoj = 0;
+	private double fDim;
+	private double ft;
+	private double arm;
+	private double bøjningsMoment;
+	
+	private double sigmaBoj;
+	
+	@Override
+	public void beregnSigmaBoj(double i, double e, double MB) {
+		sigmaBoj = (MB * e) / i;
+	}
 	
 	public double getSigmaBoj() {
 		return sigmaBoj;
 	}
 
+	//Metoder til MB
 	@Override
-	public void beregnSigmaBoj(Inertimoment i, Halvhojde e, double MB) {
+	public void setArmLaengde(double mm) {
+		arm = mm;
+		
+	}
 
-		sigmaBoj = (MB *e.getHalvhojde()) / i.getInertimoment();
+	@Override
+	public double beregnMb(double ft, double mm) {
+		bøjningsMoment = this.ft * arm;
+		return bøjningsMoment;
+	}
+
+	@Override
+	public double getMb() {
+		return bøjningsMoment;
 	}
 }
