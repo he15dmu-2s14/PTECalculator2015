6d818557ad085a49b12684248f3acd09137842a4
jgn-epp <jgn.epp@gmail.com>
2015-03-10 12:49:03 +0100

    Navneændring af sigmaref til Refencespaending osv

---
+++ 04-Implementation/PTECalculator2015/src/domain/Referencespaending.java
@@ -0,0 +1,12 @@
+package domain;
+
+public interface Referencespaending {
+  
+  public double getSigmaRef();
+  
+  public void setSigmaN(SigmaN sigman);
+  
+  public void setSigmaBoej(Bojningsmoment sigmab);
+  
+  public void setTau(Forskydningsspaending tau);
+}
