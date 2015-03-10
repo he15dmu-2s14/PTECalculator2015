585d086e87c0538ad4e28ceff95a91b4e9b508e6
LeaRicken <lea2ricken@gmail.com>
2015-03-10 14:42:31 +0100

    Ændring i klassen Normalspaending

--- 04-Implementation/PTECalculator2015/src/domain/NormalspaendingImpl.java
+++ 04-Implementation/PTECalculator2015/src/domain/NormalspaendingImpl.java
@@ -12,4 +12,11 @@ public class NormalspaendingImpl {
 	public void setAreal( Areal areal ) {
 		this.areal = areal;
 	}
+	
+	public double getNormalspaending( Normalkraft normalkraft, Areal areal ) {
+		if ( normalkraft != null && areal != null )
+			return normalkraft.getNormalkraft() / areal.getAreal();
+		else
+			return Double.NaN;
+	}
 }
