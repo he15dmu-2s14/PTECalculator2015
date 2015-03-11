4b9eaf1893f5050d69ed8dfc3df162c3f836decf
henrik broe henriksen <henrik.broe@gmail.com>
2015-03-11 08:20:01 +0100

    rettet stavefejl

--- 04-Implementation/PTECalculator2015/src/domain/Enhed.java
+++ 04-Implementation/PTECalculator2015/src/domain/Enhed.java
@@ -2,7 +2,7 @@ package domain;
 
 /**
  * Enum gennemtvinger konventionsen af, hvordan enhederne angives (saa der ikke kommer tvivl om store/smaa bogstaver)
- * Da det er typesikkert, tillader Java at switche p� disse nu 
+ * Da det er typesikkert, tillader Java at switche  paa disse nu 
  * 
  * @author Louise
  * @author AWP
